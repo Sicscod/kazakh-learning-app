fc3477961320749e5c3fef05c1411ae11d26fd93
Sicscod <tileshsanzhar@gmail.com>
2025-10-30 19:08:10 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,25 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-<<<<<<< HEAD
 class _MyAppState extends State<MyApp> {
   String currentLang = "kz";
-=======
-<<<<<<< HEAD
-
-
-class HomePage extends StatelessWidget {
-=======
-class WelcomePage extends StatefulWidget {
-  const WelcomePage({super.key});
->>>>>>> fd9e0e37a2aee3b00172f8b2147f63a93d066e9c
 
   final greetings = {"kz": "СӘЛЕМ!", "ru": "ПРИВЕТ!", "en": "HELLO!"};
-  final loginText = {"kz": "Кіру", "ru": "Войти", "en": "Login"};
-  final registerText = {"kz": "Тіркелу", "ru": "Регистрация", "en": "Register"};
+  final loginText = {"kz": "кіру", "ru": "войти", "en": "login"};
+  final registerText = {"kz": "тіркелу", "ru": "регистрация", "en": "register"};
 
   void switchLang() {
     setState(() {
-      if (currentLang == "kz") currentLang = "ru";
-      else if (currentLang == "ru") currentLang = "en";
-      else currentLang = "kz";
+      if (currentLang == "kz") {
+        currentLang = "ru";
+      } else if (currentLang == "ru") {
+        currentLang = "en";
+      } else {
+        currentLang = "kz";
+      }
     });
   }
 
->>>>>>> a5b5d29b69f3c0216888454ca84e0dc52396f1d1
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -52,57 +45,18 @@ class WelcomePage extends StatefulWidget {
               ),
             ),
 
-            // 🔹 Кнопка смены языка (lang.jpg)
-            Positioned(
-              top: 50,
-              right: 20,
-              child: GestureDetector(
-                onTap: switchLang,
-                child: Container(
-                  width: 31,
-                  height: 34,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(2),
-                    border: Border.all(color: Colors.black, width: 1),
-                    image: const DecorationImage(
-                      image: AssetImage("assets/lang.jpg"),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            // 🔹 Текст “СӘЛЕМ!”
+            // 🔹 Текст “СӘЛЕМ!” без анимации
             Positioned(
               top: 326,
               left: 178,
-              child: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 400),
-                child: Text(
-                  greetings[currentLang]!,
-                  key: ValueKey(currentLang),
-                  style: const TextStyle(
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w800,
-                    fontSize: 48,
-                    color: Color(0xFF32734E),
-                    height: 1.0,
-                  ),
-                ),
-              ),
-            ),
-
-            // 🔹 Прямоугольник под кнопкой "Тіркелу"
-            Positioned(
-              top: 398,
-              left: 178,
-              child: Container(
-                width: 197,
-                height: 47,
-                decoration: BoxDecoration(
-                  color: const Color(0xFF32734E),
-                  borderRadius: BorderRadius.circular(12),
+              child: Text(
+                greetings[currentLang]!,
+                style: const TextStyle(
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w800,
+                  fontSize: 48,
+                  color: Color(0xFF32734E),
+                  height: 1.0,
                 ),
               ),
             ),
@@ -110,15 +64,15 @@ class WelcomePage extends StatefulWidget {
             // 🔹 Кнопка “Тіркелу”
             Positioned(
               top: 403,
-              left: 222,
+              left: 178,
               child: GestureDetector(
                 onTap: () {},
                 child: Container(
-                  width: 186,
-                  height: 42,
+                  width: 197,
+                  height: 47,
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: const Color(0xFFFBB03B),
+                    color: const Color(0xFF32734E),
                     borderRadius: BorderRadius.circular(10),
                   ),
                   child: Text(
@@ -127,7 +81,7 @@ class WelcomePage extends StatefulWidget {
                       fontFamily: 'Inter',
                       fontWeight: FontWeight.w800,
                       fontSize: 27,
-                      color: Colors.white,
+                      color: Color(0xFFFBB03B),
                       height: 1.0,
                     ),
                   ),
@@ -135,32 +89,18 @@ class WelcomePage extends StatefulWidget {
               ),
             ),
 
-            // 🔹 Прямоугольник под кнопкой "Кіру"
-            Positioned(
-              top: 470,
-              left: 178,
-              child: Container(
-                width: 197,
-                height: 47,
-                decoration: BoxDecoration(
-                  color: const Color(0xFF32734E),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-            ),
-
             // 🔹 Кнопка “Кіру”
             Positioned(
               top: 475,
-              left: 249,
+              left: 178,
               child: GestureDetector(
                 onTap: () {},
                 child: Container(
-                  width: 186,
-                  height: 42,
+                  width: 197,
+                  height: 47,
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: const Color(0xFFFBB03B),
+                    color: const Color(0xFF32734E),
                     borderRadius: BorderRadius.circular(10),
                   ),
                   child: Text(
@@ -169,8 +109,28 @@ class WelcomePage extends StatefulWidget {
                       fontFamily: 'Inter',
                       fontWeight: FontWeight.w800,
                       fontSize: 27,
-                      color: Colors.white,
+                      color: Color(0xFFFBB03B),
                       height: 1.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            // 🔹 Кнопка смены языка
+            Positioned(
+              top: 550,
+              left: 344,
+              child: GestureDetector(
+                onTap: switchLang,
+                child: Container(
+                  width: 31,
+                  height: 34,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(2),
+                    image: const DecorationImage(
+                      image: AssetImage("assets/lang.jpg"),
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
